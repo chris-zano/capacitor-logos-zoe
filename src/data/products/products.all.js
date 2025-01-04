3be045484e4d8c03e48da585d04fbcb9f867d3b6
Christian Solomon <cncs.chris@gmail.com>
2025-01-04 22:35:46 +0000

    add shop and donate  pages

--- src/data/products/products.all.js
+++ src/data/products/products.all.js
@@ -1,5 +1,5 @@
 const getProducts = async () => {
-    const response = await fetch('http://localhost:3123/products');
+    const response = await fetch('https://logos-server-j2ld.onrender.com/products');
     const data = await response.json();
     return data;
 }
