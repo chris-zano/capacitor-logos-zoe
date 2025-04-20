c5cc8a4a59bb7823caf1b89f98558d7f39ee5db4
Christian Solomon <cncs.chris@gmail.com>
2025-04-20 08:17:43 +0000

    remove cached devotionals

--- src/data/devotionals/get_todays_devotional.js
+++ src/data/devotionals/get_todays_devotional.js
@@ -2,31 +2,9 @@ import BASEURL from "../../baseUrl.js";
 
 const getTodaysDevotional = async () => {
   try {
-    const key = 'todaysDevotional';
-    const cachedData = localStorage.getItem(key);
-    const today = new Date().toISOString().split('T')[0];
-
-    if (cachedData) {
-      const { data, timestamp } = JSON.parse(cachedData);
-
-
-      if (timestamp === today) {
-        return data;
-      }
-    }
-
 
     const response = await fetch(`${BASEURL}/devotionals/today`);
     const data = await response.json();
-
-
-    localStorage.setItem(
-      key,
-      JSON.stringify({
-        data,
-        timestamp: today,
-      })
-    );
 
     return data;
   } catch (error) {
